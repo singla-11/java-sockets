a34fbbf8d3c6fbf3791a7e1f5da87c5c272883c9
singla-11 <abhisheksingla709@gmail.com>
2021-07-19 02:22:49 +0530

    interrupt client execution if failure in connecting to remote server

--- client/src/main/java/com/programming/client/Client.java
+++ client/src/main/java/com/programming/client/Client.java
@@ -10,19 +10,15 @@ public class Client {
 
     private Socket socket;
 
-    public Client(String host, int port) {
-        try {
-            this.socket = new Socket(host, port);
-            log.info("Client connected to server on host: {} and port: {}", host, port);
+    public Client(String host, int port) throws IOException {
+        this.socket = new Socket(host, port);
+        log.info("Client connected to server on host: {} and port: {}", host, port);
 
-            if (log.isDebugEnabled()) {
-                log.debug("Inet Address of remote machine: {}", socket.getInetAddress());
-                log.debug("Inet Address of local machine: {}", socket.getLocalAddress());
-                log.debug("Port of remote machine: {}", socket.getPort());
-                log.debug("Port of local machine: {}", socket.getLocalPort());
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        if (log.isDebugEnabled()) {
+            log.debug("Inet Address of remote machine: {}", socket.getInetAddress());
+            log.debug("Inet Address of local machine: {}", socket.getLocalAddress());
+            log.debug("Port of remote machine: {}", socket.getPort());
+            log.debug("Port of local machine: {}", socket.getLocalPort());
         }
     }
 
--- client/src/main/java/com/programming/client/ClientManager.java
+++ client/src/main/java/com/programming/client/ClientManager.java
@@ -1,13 +1,22 @@
 package com.programming.client;
 
 import com.programming.socket.util.CommandLineArgumentUtil;
+import java.io.IOException;
 
 public class ClientManager {
 
     public static void main(String[] args) {
-        Client client =
-                new Client(
-                        CommandLineArgumentUtil.serverHost(), CommandLineArgumentUtil.serverPort());
+        Client client;
+        try {
+            client =
+                    new Client(
+                            CommandLineArgumentUtil.serverHost(),
+                            CommandLineArgumentUtil.serverPort());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
         client.sendConsoleInputToServer(CommandLineArgumentUtil.endOfStreamIdentifier());
     }
 }
