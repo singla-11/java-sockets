24fea6c493c4e4fed29a133fea514771f3ba82fa
singla-11 <abhisheksingla709@gmail.com>
2021-07-18 21:15:30 +0530

    Remove duplicate dependencies

--- script/server.sh
+++ script/server.sh
@@ -2,4 +2,4 @@
 
 cd ..
 mvn clean package
-java -Dhost=localhost -Dport=5001 -Deos=-9 -jar server/target/server-1.0-SNAPSHOT.jar
+java -Dport=5001 -Deos=-9 -jar server/target/server-1.0-SNAPSHOT.jar
